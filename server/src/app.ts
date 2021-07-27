0b947a5ba3536a1a239db728d97c70b09256e969
sanginchun <sanginchun91@gmail.com>
2021-07-27 19:09:38 +0900

    [#2] wip: loaders 추가

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import loader from './loaders';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+loader(app);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
---
+++ server/src/loaders/index.ts
@@ -0,0 +1,6 @@
+import { Express } from 'express';
+
+export default (app: Express) => {
+  // load morgan
+  console.log('morgan loaded');
+};
