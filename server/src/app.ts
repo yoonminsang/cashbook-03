c9412d77f6a1752253ebf5ed9d1b9bb9f51360f2
sanginchun <sanginchun91@gmail.com>
2021-07-27 18:45:49 +0900

    [#2] chore: dotenv 설정

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
