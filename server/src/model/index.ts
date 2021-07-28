c44943c469258364412bf4e3858754e616628a84
sanginchun <sanginchun91@gmail.com>
2021-07-28 20:20:42 +0900

    [#2] wip: Model간 Relation 설정

--- server/src/model/index.ts
+++ server/src/model/index.ts
@@ -1,7 +1,7 @@
-import { initCategory } from './category';
-import { initUser } from './user';
-import { initPayment } from './payment';
-import { initAccount } from './account';
+import Category, { initCategory } from './category';
+import User, { initUser } from './user';
+import Payment, { initPayment } from './payment';
+import Account, { initAccount } from './account';
 
 export const modelInitFunctions: Function[] = [
   initCategory,
@@ -9,3 +9,11 @@ export const modelInitFunctions: Function[] = [
   initPayment,
   initAccount,
 ];
+
+export const setRelations = () => {
+  Category.hasMany(Account, { foreignKey: 'category_id' });
+  User.hasMany(Account, { foreignKey: 'user_id', onDelete: 'cascade' });
+  Payment.hasMany(Account, { foreignKey: 'payment_id', onDelete: 'cascade' });
+
+  Payment.belongsTo(User, { foreignKey: 'user_id', onDelete: 'cascade' });
+};
