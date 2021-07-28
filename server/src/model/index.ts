92c38416aebed31217ad803462fbe8d2f8fd9f4c
sanginchun <sanginchun91@gmail.com>
2021-07-28 17:13:07 +0900

    [#2] wip: User 모델 초기화 추가

--- server/src/model/index.ts
+++ server/src/model/index.ts
@@ -1,3 +1,4 @@
 import { initCategory } from './category';
+import { initUser } from './user';
 
-export const modelInitFunctions: [Function] = [initCategory];
+export const modelInitFunctions: Function[] = [initCategory, initUser];
