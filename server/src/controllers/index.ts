32613b9796ed93c973dace68a8702525440785e7
sanginchun <sanginchun91@gmail.com>
2021-07-28 16:12:02 +0900

    [#2] wip: 잘못된 api endpoint 에러 처리

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,10 +1,15 @@
-import { Router } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
+import { ErrorStatus } from '../error';
 import CategoryController from './category';
 
 export default () => {
   const router = Router();
 
   router.use('/category', new CategoryController().configureRoutes());
+
+  router.use((req: Request, res: Response, next: NextFunction) =>
+    next(new ErrorStatus(404, 'Unknown Endpoint')),
+  );
 
   return router;
 };
