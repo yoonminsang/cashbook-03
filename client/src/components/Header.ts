b90a066bb99f30b78369e601a6255f71332d2803
yoonminsang <apdlyooapdl@naver.com>
2021-07-28 18:47:02 +0900

    [#10] wip: 헤더 뷰 예시
    - innerhtml을 사용하기 때문에 특별한 코드 없이 오직 뷰만 존재

---
+++ client/src/components/Header.ts
@@ -0,0 +1,13 @@
+class Header {
+  render({ date, user }) {
+    return `
+    <div class="year">${date && date.year}</div>
+    <div class="month">${date && date.month}</div>
+    <a href="/">메인</a>
+    <a href="/calendar">달력</a>
+    <a href="/statistics">통계</a>
+    <a href="/abc">오류</a>
+    `;
+  }
+}
+export default Header;
