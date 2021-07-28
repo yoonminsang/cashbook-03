40ae8d1538a810427586557a41be2d3f064f2475
sanginchun <sanginchun91@gmail.com>
2021-07-28 18:04:09 +0900

    [#2] wip: Account 모델 생성 및 초기화

--- server/src/model/index.ts
+++ server/src/model/index.ts
@@ -1,9 +1,11 @@
 import { initCategory } from './category';
 import { initUser } from './user';
 import { initPayment } from './payment';
+import { initAccount } from './account';
 
 export const modelInitFunctions: Function[] = [
   initCategory,
   initUser,
   initPayment,
+  initAccount,
 ];
