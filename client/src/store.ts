efcf2ad9608e90b87c149764b7c9c84b4d20690d
yoonminsang <apdlyooapdl@naver.com>
2021-07-28 13:59:23 +0900

    [#10] wip: store 수정
    - store에 옵저버패턴을 적용
    - 전역에서 사용하기 때문에 클래스가 아닌 클로저

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,15 +1,37 @@
 export const store = (function () {
   const state = {};
+  const observer = {};
   return {
     setState(type, nextState) {
       state[type] = nextState;
+      this.noify(type);
     },
     getState(type) {
       return state[type];
     },
+    subscribe(type, identifier, cb) {
+      if (!observer[type]) {
+        observer[type] = [];
+      }
+      observer[type].push({
+        identifier,
+        cb: () => cb(state[type], this.getState(type)),
+      });
+      this.notify(type);
+    },
+    unsubscribe(type, identifier) {
+      observer[type] = observer[type].filter(
+        ({ identifier: idf }) => idf === identifier,
+      );
+    },
+    notify(type) {
+      if (observer[type]) observer[type].forEach(({ cb }) => cb());
+    },
   };
 })();
 
-// 사용법
-//Store.setState('user', { id: 1, nickname: '민상', email: 'woowa@' });
-//console.log(Store.getState('user'));
+export const GLOBALSTATE = {
+  date: 'date',
+  user: 'user',
+  data: 'data',
+};
