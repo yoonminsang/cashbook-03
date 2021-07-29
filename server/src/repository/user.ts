089f5003b0c3598c6a2a2b6dcf7830ea14264d58
yoonminsang <apdlyooapdl@naver.com>
2021-07-29 12:38:36 +0900

    [#3] feat: 회원가입 레포지터리 추가(쿼리문-sequelize)
    - bcrypt와 uuid 적용

--- server/src/repository/user.ts
+++ server/src/repository/user.ts
@@ -1,7 +1,28 @@
 import User from '../model/user';
+import bcrypt from 'bcrypt';
+import { v4 as uuidv4 } from 'uuid';
 
 export default class UserRepository {
   async get(id: string) {
     return await User.findByPk(id);
+  }
+
+  async getEmail(email: string) {
+    return await User.findOne({
+      where: {
+        email: email,
+      },
+    });
+  }
+
+  async insertUser(email: string, password: string, nickname: string) {
+    const id = uuidv4();
+    const hash = await bcrypt.hash(password + '', 10);
+    return await User.create({
+      id,
+      email,
+      password: hash,
+      nickname,
+    });
   }
 }
