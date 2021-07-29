f09a8e51efc12b99a7ff1b5147f3276e303290e2
sanginchun <sanginchun91@gmail.com>
2021-07-30 00:00:38 +0900

    [#16] wip: Login, Signup 페이지 추가, 라우터 설정

---
+++ client/src/pages/Login.ts
@@ -0,0 +1,14 @@
+class Login {
+  $app: HTMLElement;
+  constructor({ $app }) {
+    this.$app = $app;
+    this.render();
+  }
+  render = () => {
+    const $fragment = document.createDocumentFragment();
+    const $div = document.createElement('div');
+    this.$app.innerHTML = '';
+    this.$app.appendChild($fragment);
+  };
+}
+export default Login;
