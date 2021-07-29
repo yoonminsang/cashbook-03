073d0ccda6eeb2b5a92bc957f3cadff3fa6333b6
yoonminsang <apdlyooapdl@naver.com>
2021-07-29 18:29:04 +0900

    [#3] feat: 패스포트 로컬 로그인
    -

---
+++ server/src/passport/local.ts
@@ -0,0 +1,32 @@
+import passport from 'passport';
+import Strategy from 'passport-local';
+import bcrypt from 'bcrypt';
+import UserRepository from '../repository/user';
+
+const LocalStrategy = Strategy.Strategy;
+const userRepository = new UserRepository();
+
+export default () => {
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: 'eamil',
+        passwordField: 'password',
+      },
+      async (email, password, done) => {
+        try {
+          console.log('local strategy', email);
+          const user: any = await userRepository.getByEmail(email);
+          if (user) {
+            const match = await bcrypt.compare(password, user.password);
+            if (match) return done(null, user);
+            return done(null, false);
+          }
+          return done(null, false);
+        } catch (e) {
+          console.error('local passport error', e);
+        }
+      },
+    ),
+  );
+};
