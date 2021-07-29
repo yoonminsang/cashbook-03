ead0aee3c287f776347eabbfa98b288bc7e0ec63
sanginchun <sanginchun91@gmail.com>
2021-07-29 17:44:37 +0900

    [#17] wip: 달력, 차트 페이지 헤더 코드 작성

--- client/src/pages/Calendar.ts
+++ client/src/pages/Calendar.ts
@@ -1,2 +1,18 @@
-class Calendar {}
+import HeaderContainer from '../containers/HeaderContainer';
+
+class Calendar {
+  $app: HTMLElement;
+  constructor({ $app }) {
+    this.$app = $app;
+    this.render();
+  }
+  render = () => {
+    const $fragment = document.createDocumentFragment();
+    const $header = document.createElement('header');
+    const headerContainer = new HeaderContainer({ $target: $header });
+    $fragment.append(headerContainer.html);
+    this.$app.innerHTML = '';
+    this.$app.appendChild($fragment);
+  };
+}
 export default Calendar;
--- client/src/pages/Statistics.ts
+++ client/src/pages/Statistics.ts
@@ -1,2 +1,18 @@
-class Statistics {}
+import HeaderContainer from '../containers/HeaderContainer';
+
+class Statistics {
+  $app: HTMLElement;
+  constructor({ $app }) {
+    this.$app = $app;
+    this.render();
+  }
+  render = () => {
+    const $fragment = document.createDocumentFragment();
+    const $header = document.createElement('header');
+    const headerContainer = new HeaderContainer({ $target: $header });
+    $fragment.append(headerContainer.html);
+    this.$app.innerHTML = '';
+    this.$app.appendChild($fragment);
+  };
+}
 export default Statistics;
