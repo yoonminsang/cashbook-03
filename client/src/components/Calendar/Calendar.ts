0ca2bcfb0711a6b861d861a608548016f644637d
sanginchun <sanginchun91@gmail.com>
2021-08-01 22:08:35 +0900

    [#14] wip: 하단 텍스트 추가

--- client/src/components/Calendar/Calendar.ts
+++ client/src/components/Calendar/Calendar.ts
@@ -76,6 +76,11 @@ const Calendar = ({}) => {
           </div>
         </div>
       </div>
+      <div class="data">
+        <div class="data__income">총 수입: 1,822,000</div>
+        <div>총 지출: 812,000</div>
+        <div class="data__total">총 수입: 1,010,000</div>
+      </div>
     </div>
   `;
 };
