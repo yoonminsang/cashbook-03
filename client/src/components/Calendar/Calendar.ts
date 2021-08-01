2e6dc58015adbab97403639539da93f75564bf44
sanginchun <sanginchun91@gmail.com>
2021-08-01 21:11:00 +0900

    [#14] wip: Calendar 탭 요일 스타일링

--- client/src/components/Calendar/Calendar.ts
+++ client/src/components/Calendar/Calendar.ts
@@ -1,6 +1,18 @@
 const Calendar = ({}) => {
-  return `
-    <div>Calendar</div>
-    `;
+  return /*html*/ `
+    <div class="calendar-tab">
+      <div class="calendar">
+        <div class="calendar__week">
+          <div class="calendar__week__day">일</div>
+          <div class="calendar__week__day">월</div>
+          <div class="calendar__week__day">화</div>
+          <div class="calendar__week__day">수</div>
+          <div class="calendar__week__day">목</div>
+          <div class="calendar__week__day">금</div>
+          <div class="calendar__week__day">토</div>
+        </div>
+      </div>
+    </div>
+  `;
 };
 export default Calendar;
