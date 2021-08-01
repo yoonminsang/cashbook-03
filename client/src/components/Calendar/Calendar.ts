a88e214685487a2ced72885e98397f30256080e3
sanginchun <sanginchun91@gmail.com>
2021-08-01 21:16:26 +0900

    [#14] wip: Calendar 탭 클래스명 단순하게 변경

--- client/src/components/Calendar/Calendar.ts
+++ client/src/components/Calendar/Calendar.ts
@@ -2,14 +2,14 @@ const Calendar = ({}) => {
   return /*html*/ `
     <div class="calendar-tab">
       <div class="calendar">
-        <div class="calendar__week">
-          <div class="calendar__week__day">일</div>
-          <div class="calendar__week__day">월</div>
-          <div class="calendar__week__day">화</div>
-          <div class="calendar__week__day">수</div>
-          <div class="calendar__week__day">목</div>
-          <div class="calendar__week__day">금</div>
-          <div class="calendar__week__day">토</div>
+        <div class="calendar__header">
+          <div>일</div>
+          <div>월</div>
+          <div>화</div>
+          <div>수</div>
+          <div>목</div>
+          <div>금</div>
+          <div>토</div>
         </div>
       </div>
     </div>
