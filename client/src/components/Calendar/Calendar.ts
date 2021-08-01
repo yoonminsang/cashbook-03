3e748b39d8b70c8a45a59cdb421e7e9df4e55b94
sanginchun <sanginchun91@gmail.com>
2021-08-01 21:57:39 +0900

    [#14] wip: Calendar 스타일링

--- client/src/components/Calendar/Calendar.ts
+++ client/src/components/Calendar/Calendar.ts
@@ -11,6 +11,70 @@ const Calendar = ({}) => {
           <div>금</div>
           <div>토</div>
         </div>
+        <div class="calendar__content">
+          <div class="week">
+            <div class="week__day">
+              <div class="week__day__income">1,822,480</div>
+              <div class="week__day__expenditure">-9,500</div>
+              <div class="week__day__total">1,812,980</div>
+              <div class="week__day__date">1</div>
+            </div>
+            <div class="week__day today">
+              <div class="week__day__date">2</div>
+            </div>
+            <div class="week__day">
+              <div class="week__day__date">3</div>
+            </div>
+            <div class="week__day">
+              <div class="week__day__date">4</div>
+            </div>
+            <div class="week__day">
+              <div class="week__day__date">5</div>
+            </div>
+            <div class="week__day">
+              <div class="week__day__date">6</div>
+            </div>
+            <div class="week__day">
+              <div class="week__day__date">7</div>
+            </div>
+          </div>
+          <div class="week">
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+          </div>
+          <div class="week">
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+          </div>
+          <div class="week">
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+          </div>
+          <div class="week">
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+            <div class="week__day"></div>
+          </div>
+        </div>
       </div>
     </div>
   `;
