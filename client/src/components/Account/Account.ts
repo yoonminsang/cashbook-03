011443a2c1a92a932fb096609b7fc701504271c7
yoonminsang <apdlyooapdl@naver.com>
2021-08-03 11:53:59 +0900

    [#58] wip: 메인 account div를 ul, li로 수정

--- client/src/components/Account/Account.ts
+++ client/src/components/Account/Account.ts
@@ -16,15 +16,15 @@ const Account = ({}) => {
         <div class="left"><div class="date">7월 15일 </div><div class="day">목</div></div>
         <div class="right"><div>수입</div><div>15,000</div><div>지출</div><div>20,000</div></div>
       </div>
-      <div class="account-item-content">
-        <div class="account-item-inner">
+      <ul class="account-item-content">
+        <li class="account-item-inner">
           <button class="category">문화/여가</button>
           <div class="content">스트리밍 정기 결제</div>
           <div class="payment">현대카드</div>
           <div class="price">-10,900원</div>
           <button class="wci wci-close js-remove-account"></button>
-        </div>
-      </div>
+        </li>
+      </ul>
     </div>
     `;
 };
