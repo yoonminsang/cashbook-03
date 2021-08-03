00976d2c937c902914517bc0ae0c95278a1e375b
sanginchun <sanginchun91@gmail.com>
2021-08-03 17:33:28 +0900

    [#20] wip: 캘린더 컴포넌트 일자 채워주기

--- client/src/components/Calendar/CalendarContent/CalendarContent.ts
+++ client/src/components/Calendar/CalendarContent/CalendarContent.ts
@@ -2,6 +2,8 @@ export const CalendarContent = ({ date }) => {
   const $target = document.createElement('div');
   $target.innerHTML = initialMarkup();
 
+  fillDates($target, date);
+
   return $target.innerHTML;
 };
 
@@ -30,3 +32,37 @@ const week = () => /*html*/ `
 const weekDayDate = (date: number) => /*html*/ `
   <div class="week__day__date" data-date=${date}>${date}</div>
 `;
+
+const fillDates = (
+  $target: HTMLElement,
+  { year, month }: { year: number; month: number },
+) => {
+  const firstDayIndex = new Date(year, month - 1).getDay();
+  const lastDate = new Date(year, month, 0).getDate();
+
+  const $dates = $target.querySelectorAll('.week__day');
+
+  for (let index = firstDayIndex, date = 1; date <= lastDate; index++, date++) {
+    if ($dates[index])
+      $dates[index].insertAdjacentHTML('afterbegin', weekDayDate(date));
+    else {
+      // add another week
+      $target
+        .querySelector('.calendar__content')
+        .insertAdjacentHTML('beforeend', week());
+
+      const $lastWeekDates = $target.querySelectorAll(
+        '.week:last-child > .week__day',
+      );
+
+      for (let newIndex = 0; date <= lastDate; newIndex++, date++) {
+        $lastWeekDates[newIndex].insertAdjacentHTML(
+          'afterbegin',
+          weekDayDate(date),
+        );
+      }
+
+      break;
+    }
+  }
+};
