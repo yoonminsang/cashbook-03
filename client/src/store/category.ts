47b32dff74f61689680e5dbaabbde739240e25d5
sanginchun <sanginchun91@gmail.com>
2021-08-04 19:29:44 +0900

    [#53][#54] wip: category 관련코드 수정

--- client/src/store/category.ts
+++ client/src/store/category.ts
@@ -4,20 +4,11 @@ import { getCategory } from '../utils/api/category';
 class Payment extends Observable {
   async init() {
     try {
-      const {
-        data: { data },
-      } = await getCategory();
-
+      const data = await getCategory();
       this.setState(data);
-    } catch (e) {
-      // const {
-      //   response: {
-      //     data: { message },
-      //   },
-      // } = e;
-      const message = e.response && e.response.data && e.response.data.message;
-      if (message) console.error(message);
-      console.error(e);
+    } catch (message) {
+      // alert(message);
+      return;
     }
   }
 }
