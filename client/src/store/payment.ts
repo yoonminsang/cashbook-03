d4d34e7e9223bddb0e8c56312ecc83df3ebad58b
sanginchun <sanginchun91@gmail.com>
2021-08-04 19:20:54 +0900

    [#53][#54] wip: payment store 수정 - UI적으로 에러 또는 추가 성공 메시지를 보여주면 좋을 듯 함 (account도 마찬가지) - 모달창 같은게 임시로 보여지고 2~3초 후에 자동으로 사라지는 것을 고려해보았지만 시간이 없을 것 같아서 일단 필수로 보여줘야 할 오류만 alert로 처리함

--- client/src/store/payment.ts
+++ client/src/store/payment.ts
@@ -4,65 +4,37 @@ import { getPayment, setPayment, removePayment } from '../utils/api/payment';
 class Payment extends Observable {
   async init() {
     try {
-      const {
-        data: { data },
-      } = await getPayment();
-
+      const data = await getPayment();
       this.setState(data);
-    } catch (e) {
-      // const {
-      //   response: {
-      //     data: { message },
-      //   },
-      // } = e;
-      const message = e.response && e.response.data && e.response.data.message;
-      if (message) console.error(message);
-      console.error(e);
+    } catch (message) {
+      // alert(message);
+      return;
     }
   }
 
   async update() {
-    console.log('re-fetching payment..');
     await this.init();
   }
 
   async add({ name }) {
     try {
-      const {
-        data: { message },
-      } = await setPayment({ name });
+      await setPayment({ name });
 
-      console.log(message);
       this.update();
-    } catch (e) {
-      // const {
-      //   response: {
-      //     data: { message },
-      //   },
-      // } = e;
-      const message = e.response && e.response.data && e.response.data.message;
-      if (message) console.log(message);
-      console.error(e);
+    } catch (message) {
+      // alert(message);
+      return;
     }
   }
 
   async remove({ id }) {
     try {
-      const {
-        data: { message },
-      } = await removePayment({ id });
+      await removePayment({ id });
 
-      console.log(message);
       this.update();
-    } catch (e) {
-      // const {
-      //   response: {
-      //     data: { message },
-      //   },
-      // } = e;
-      const message = e.response && e.response.data && e.response.data.message;
-      if (message) console.log(message);
-      console.error(e);
+    } catch (message) {
+      alert(message);
+      return;
     }
   }
 }
