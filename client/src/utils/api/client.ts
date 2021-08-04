134361817efb0a63b6c870c10750f1e6ddd84a98
yoonminsang <apdlyooapdl@naver.com>
2021-08-04 09:55:27 +0900

    [#63] bug: cors 설정

--- client/src/utils/api/client.ts
+++ client/src/utils/api/client.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const client = axios.create();
 
+client.defaults.baseURL = `${location.host}:3000`;
+client.defaults.withCredentials = true;
 /*
   글로벌 설정 예시:
   // API 주소를 다른 곳으로 사용함
--- server/src/config/cors-config.ts
+++ server/src/config/cors-config.ts
@@ -1,3 +1,6 @@
 import { CorsOptions } from 'cors';
 
-export const corsOptions: CorsOptions = {};
+export const corsOptions: CorsOptions = {
+  origin: process.env.FRONT_URL,
+  credentials: true,
+};
