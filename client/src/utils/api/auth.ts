49c89407c79c9eae7f8aae688072ec04f9873eff
sanginchun <sanginchun91@gmail.com>
2021-08-04 17:29:56 +0900

    [#53][#54] wip: auth api 수정 - axios에서 추상화된 메소드 사용 - 에러 코드별 메시지 상수화 - 요청 실패시 처리 로직 개별적으로 수정

--- client/src/utils/api/auth.ts
+++ client/src/utils/api/auth.ts
@@ -1,15 +1,52 @@
-import client from './client';
+import { request } from './client';
+
+const ERROR_MESSAGE = {
+  400: '아이디 또는 비밀번호가 일치하지 않습니다',
+  409: '이미 가입된 이메일입니다',
+  500: '오류가 발생했습니다. 잠시 후 다시 시도하세요',
+};
 
 // 로그인
-export const login = ({ email, password }) =>
-  client.post('/api/auth/login', { email, password });
+export const login = async ({ email, password }) => {
+  try {
+    return await request('post', '/api/auth/login', { email, password });
+  } catch (statusCode) {
+    throw ERROR_MESSAGE[statusCode];
+  }
+};
 
 // 회원가입
-export const signup = ({ email, nickname, password }) =>
-  client.post('/api/user/signup', { email, nickname, password });
+export const signup = async ({ email, nickname, password }) => {
+  try {
+    return await request('post', '/api/user/signup', {
+      email,
+      nickname,
+      password,
+    });
+  } catch (statusCode) {
+    throw ERROR_MESSAGE[statusCode];
+  }
+};
 
 // 로그인 상태 확인
-export const check = () => client.get('/api/auth/check');
+export const check = async () => {
+  try {
+    const response = await request('get', '/api/auth/check');
+    return response.data.user;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
 
 // 로그아웃
-export const logout = () => client.post('/api/auth/logout');
+export const logout = async () => {
+  try {
+    await request('post', '/api/auth/logout');
+
+    return true;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+};
