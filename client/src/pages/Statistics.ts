11d6b3b0dba6bae7f8d1390bc638788a2cb638d1
sanginchun <sanginchun91@gmail.com>
2021-08-05 11:08:31 +0900

    [#22] chore: page에서 꺾은선 그래프 넣는 로직 제거

--- client/src/pages/Statistics.ts
+++ client/src/pages/Statistics.ts
@@ -1,6 +1,5 @@
 import MainChartContainer from '../containers/MainChartContainer';
 import HeaderContainer from '../containers/HeaderContainer';
-import LineChart from '../components/LineChart/LineChart';
 
 class Statistics {
   $app: HTMLElement;
@@ -17,9 +16,6 @@ class Statistics {
     const mainChartContainer = new MainChartContainer({ $target: $donut });
     $fragment.append(headerContainer.html, mainChartContainer.html);
 
-    const $dummy = document.createElement('div');
-    $dummy.innerHTML = LineChart({ accountByCategory: [] });
-    $fragment.append($dummy.firstElementChild);
     this.$app.innerHTML = '';
     this.$app.appendChild($fragment);
   };
