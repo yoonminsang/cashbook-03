330250de155d0903e0e5807aa78f0e4ff587371e
sanginchun <sanginchun91@gmail.com>
2021-08-05 10:31:30 +0900

    [#15][#22] wip: 카테고리별로 내역 가져오기 수정(최근 6개월로)

--- client/src/utils/api/account.ts
+++ client/src/utils/api/account.ts
@@ -21,13 +21,13 @@ export const getAccount = async ({ year, month }) => {
 };
 
 // 카테고리별 account 불러오기
-export const getAccountByCategory = async ({ year, categoryId }) => {
+export const getAccountByCategory = async ({ year, month, categoryId }) => {
   try {
     const {
       data: { data },
     } = await request(
       'get',
-      `/api/account?year=${year}&categoryId=${categoryId}`,
+      `/api/account?year=${year}&month=${month}&categoryId=${categoryId}`,
     );
     return data;
   } catch (statusCode) {
